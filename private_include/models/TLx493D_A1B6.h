15e4ba22456a8d2971e9c859258ba0affd689dc0
Ri Teii <teiiri@outlook.com>
2025-03-11 23:11:45 +0000

    🛠️ Implement `TLx493D_A1B6` functions

--- private_include/models/TLx493D_A1B6.h
+++ private_include/models/TLx493D_A1B6.h
@@ -29,28 +29,28 @@ bool TLx493D_A1B6_deinit(TLx493D_t* sensor);
 bool TLx493D_A1B6_readRegisters(TLx493D_t* sensor);
 bool TLx493D_A1B6_readRegistersAndCheck(TLx493D_t* sensor);
 
-void TLx493D_A1B6_calculateRawTemperature(TLx493D_t* sensor, int16_t* temperature);
+void TLx493D_A1B6_calculateRawTemperature(const TLx493D_t* sensor, int16_t* temperature);
 bool TLx493D_A1B6_getRawTemperature(TLx493D_t* sensor, int16_t* temperature);
 
-void TLx493D_A1B6_calculateRawMagneticField(TLx493D_t* sensor, int16_t* x, int16_t* y, int16_t* z);
+void TLx493D_A1B6_calculateRawMagneticField(const TLx493D_t* sensor, int16_t* x, int16_t* y, int16_t* z);
 bool TLx493D_A1B6_getRawMagneticField(TLx493D_t* sensor, int16_t* x, int16_t* y, int16_t* z);
 
 void TLx493D_A1B6_calculateRawMagneticFieldAndTemperature(
-    TLx493D_t* sensor, int16_t* x, int16_t* y, int16_t* z, int16_t* temperature
+    const TLx493D_t* sensor, int16_t* x, int16_t* y, int16_t* z, int16_t* temperature
 );
 bool TLx493D_A1B6_getRawMagneticFieldAndTemperature(
     TLx493D_t* sensor, int16_t* x, int16_t* y, int16_t* z, int16_t* temperature
 );
 
 
-void TLx493D_A1B6_calculateTemperature(TLx493D_t* sensor, double* temp);
+void TLx493D_A1B6_calculateTemperature(const TLx493D_t* sensor, double* temp);
 bool TLx493D_A1B6_getTemperature(TLx493D_t* sensor, double* temp);
 
-void TLx493D_A1B6_calculateMagneticField(TLx493D_t* sensor, double* x, double* y, double* z);
+void TLx493D_A1B6_calculateMagneticField(const TLx493D_t* sensor, double* x, double* y, double* z);
 bool TLx493D_A1B6_getMagneticField(TLx493D_t* sensor, double* x, double* y, double* z);
 
 void TLx493D_A1B6_calculateMagneticFieldAndTemperature(
-    TLx493D_t* sensor, double* x, double* y, double* z, double* temp
+    const TLx493D_t* sensor, double* x, double* y, double* z, double* temp
 );
 bool TLx493D_A1B6_getMagneticFieldAndTemperature(
     TLx493D_t* sensor, double* x, double* y, double* z, double* temp
@@ -58,23 +58,23 @@ bool TLx493D_A1B6_getMagneticFieldAndTemperature(
 
 bool TLx493D_A1B6_setMeasurement(TLx493D_t* sensor, TLx493D_MeasurementType_t mVals);
 
-bool TLx493D_A1B6_setTrigger(TLx493D_t* sensor, TLx493D_TriggerType_t trig);
-bool TLx493D_A1B6_setSensitivity(TLx493D_t* sensor, TLx493D_SensitivityType_t sens);
+bool TLx493D_A1B6_setTrigger(const TLx493D_t* sensor, TLx493D_TriggerType_t trig);
+bool TLx493D_A1B6_setSensitivity(const TLx493D_t* sensor, TLx493D_SensitivityType_t sens);
 
 
 bool TLx493D_A1B6_setDefaultConfig(TLx493D_t* sensor);
 bool TLx493D_A1B6_setIICAddress(TLx493D_t* sensor, TLx493D_IICAddressType_t addr);
 
-bool TLx493D_A1B6_enable1ByteReadMode(TLx493D_t* sensor);
+bool TLx493D_A1B6_enable1ByteReadMode(const TLx493D_t* sensor);
 
-bool TLx493D_A1B6_enableCollisionAvoidance(TLx493D_t* sensor);
-bool TLx493D_A1B6_disableCollisionAvoidance(TLx493D_t* sensor);
+bool TLx493D_A1B6_enableCollisionAvoidance(const TLx493D_t* sensor);
+bool TLx493D_A1B6_disableCollisionAvoidance(const TLx493D_t* sensor);
 
 bool TLx493D_A1B6_enableInterrupt(TLx493D_t* sensor);
 bool TLx493D_A1B6_disableInterrupt(TLx493D_t* sensor);
 
 bool TLx493D_A1B6_setPowerMode(TLx493D_t* sensor, TLx493D_PowerModeType_t mode);
-bool TLx493D_A1B6_setUpdateRate(TLx493D_t* sensor, TLx493D_UpdateRateType_t rate);
+bool TLx493D_A1B6_setUpdateRate(const TLx493D_t* sensor, TLx493D_UpdateRateType_t rate);
 
 
 bool TLx493D_A1B6_hasValidData(const TLx493D_t* sensor);
@@ -82,29 +82,29 @@ bool TLx493D_A1B6_isFunctional(const TLx493D_t* sensor);
 
 bool TLx493D_A1B6_hasWakeUp(const TLx493D_t* sensor);
 bool TLx493D_A1B6_isWakeUpEnabled(const TLx493D_t* sensor);
-bool TLx493D_A1B6_enableWakeUpMode(TLx493D_t* sensor);
-bool TLx493D_A1B6_disableWakeUpMode(TLx493D_t* sensor);
+bool TLx493D_A1B6_enableWakeUpMode(const TLx493D_t* sensor);
+bool TLx493D_A1B6_disableWakeUpMode(const TLx493D_t* sensor);
 
 bool TLx493D_A1B6_setWakeUpThresholdsAsInteger(
-    TLx493D_t* sensor,
+    const TLx493D_t* sensor,
     int16_t xlTh, int16_t xhTh,
     int16_t ylTh, int16_t yhTh,
     int16_t zlTh, int16_t zhTh
 );
 bool TLx493D_A1B6_setWakeUpThresholds(
-    TLx493D_t* sensor,
+    const TLx493D_t* sensor,
     double temperature,
     double xLow, double xHigh,
     double yLow, double yHigh,
     double zLow, double zHigh
 );
 
-bool TLx493D_A1B6_softwareReset(TLx493D_t* sensor);
+bool TLx493D_A1B6_softwareReset(const TLx493D_t* sensor);
 
 
 uint8_t TLx493D_A1B6_calculateFuseParity(const TLx493D_t* sensor);
 uint8_t TLx493D_A1B6_calculateBusParity(const TLx493D_t* sensor);
-uint8_t TLx493D_A1B6_calculateConfigurationParity(TLx493D_t* sensor);
+uint8_t TLx493D_A1B6_calculateConfigurationParity(const TLx493D_t* sensor);
 
 bool TLx493D_A1B6_hasValidTBit(const TLx493D_t* sensor);
 bool TLx493D_A1B6_hasValidIICadr(const TLx493D_t* sensor);
@@ -116,8 +116,8 @@ bool TLx493D_A1B6_hasValidConfigurationParity(const TLx493D_t* sensor);
 bool TLx493D_A1B6_hasValidWakeUpParity(const TLx493D_t* sensor);
 bool TLx493D_A1B6_isInTestMode(const TLx493D_t* sensor);
 
-void TLx493D_A1B6_setResetValues(TLx493D_t* sensor);
-void TLx493D_A1B6_setReservedRegisterValues(TLx493D_t* sensor);
+void TLx493D_A1B6_setResetValues(const TLx493D_t* sensor);
+void TLx493D_A1B6_setReservedRegisterValues(const TLx493D_t* sensor);
 
 uint8_t TLx493D_A1B6_selectIICAddress(const TLx493D_t* sensor, TLx493D_IICAddressType_t addr);
 
@@ -134,8 +134,8 @@ bool TLx493D_A1B6_transferWriteRegisters(TLx493D_t* sensor);
 bool TLx493D_A1B6_enableParityTest(TLx493D_t* sensor);
 bool TLx493D_A1B6_disableParityTest(TLx493D_t* sensor);
 
-uint8_t TLx493D_A1B6_returnBitfield(TLx493D_t* sensor, uint8_t bitField);
-void TLx493D_A1B6_setBitfield(TLx493D_t* sensor, uint8_t bitField, uint8_t newBitFieldValue);
+uint8_t TLx493D_A1B6_returnBitfield(const TLx493D_t* sensor, uint8_t bitField);
+void TLx493D_A1B6_setBitfield(const TLx493D_t* sensor, uint8_t bitField, uint8_t newBitFieldValue);
 
 bool TLx493D_A1B6_hasValidPDBit(const TLx493D_t* sensor);
 
