b2ffe19f035dcc63e37fa1d15572752eba7d7a17
Ri Teii <teiiri@outlook.com>
2025-04-13 19:25:24 +0100

    🛠️ Switch from ESP logging to standard printf

--- port/esp_tlx493d_logger.c
+++ port/esp_tlx493d_logger.c
@@ -12,11 +12,11 @@
 #include "esp_log.h"
 
 
-static const char* TAG = "TLx493D_LOGGER";
+static const char* TAG = "tlx493d_logger";
 
 const uint16_t LOGGER_BUFFER_SIZE = 512U;
 
-static void logMessage(const char* prefix, const char* format, va_list vaList) {
+static void logMessage(const char* prefix, const char* format, const va_list vaList) {
     char buffer[LOGGER_BUFFER_SIZE];
 
     const size_t prefixSize = strlen(prefix);
@@ -27,28 +27,26 @@ static void logMessage(const char* prefix, const char* format, va_list vaList) {
         ESP_LOGE(TAG, "FATAL: Buffer overflow (> %d characters) because message too long!", LOGGER_BUFFER_SIZE);
     }
 
-    ESP_LOGI(TAG, "%s", buffer);
+    printf("%s", buffer);
 }
 
 void logPrintRegisters(const TLx493D_t* sensor, const char* headLine) {
-    ESP_LOGI(TAG, "");
+    printf("\n");
 
     if (headLine != NULL) {
-        ESP_LOGI(TAG, "%s", headLine);
+        printf("%s", headLine);
     }
 
     for (uint8_t i = 0; i < sensor->regMapSize; ++i) {
-        ESP_LOGI(TAG, "    0x%02X", sensor->regMap[i]);
+        logPrint("    0x%02X", sensor->regMap[i]);
     }
 
-    ESP_LOGI(TAG, "");
+    printf("\n");
 }
-
 
 void logPrintDouble(double d) {
-    ESP_LOGI(TAG, "%f", d);
+    printf("%f", d);
 }
-
 
 void logPrint(const char* format, ...) {
     va_list ap;
@@ -58,13 +56,14 @@ void logPrint(const char* format, ...) {
 }
 
 void logPrintln(const char* prefix, const char* format, ...) {
-    ESP_LOGI(TAG, "");
     va_list ap;
     va_start(ap, format);
     logMessage(prefix, format, ap);
     va_end(ap);
-    ESP_LOGI(TAG, "");
+
+    printf("\n");
 }
 
 void logFlush() {
+    fflush(stdout);
 }
