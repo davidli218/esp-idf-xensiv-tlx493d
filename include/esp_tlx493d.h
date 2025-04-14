f6be827268934e186d2287276959352dc8fca845
Ri Teii <teiiri@outlook.com>
2025-04-14 08:23:39 +0100

    🛠️ Include additional header

--- include/esp_tlx493d.h
+++ include/esp_tlx493d.h
@@ -3,6 +3,7 @@
 /** project includes. */
 #include "../tlx493d-arduino/src/tlx493d.h"
 #include "../tlx493d-arduino/src/tlx493d_types.h"
+#include "../tlx493d-arduino/src/tlx493d_enums.h"
 
 /** ESP-IDF port includes. */
 #include "esp_tlx493d_com_iic.h"
